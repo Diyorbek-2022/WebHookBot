d7f50440e30c34fad53245c7e105556be8d51909
Diyorbek-2022 <111636587+Diyorbek-2022@users.noreply.github.com>
2025-07-31 19:05:18 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TELEGRAM_BOT_TOKEN = "6299967361:AAHRT6s7NhLWw6U3ZPGmKCKzkDLD9IFEpsM"
-NGROK_TUNNEL_URL = "https://aiogram-bots.free.nf"
+NGROK_TUNNEL_URL = "https://webhookbot-zunx.onrender.com"
 
 # uvicorn main:app --host 0.0.0.0 --port 8000 --reload
