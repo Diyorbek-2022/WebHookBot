2ae9debdd9d11cdffdb0cc535c14911cc4530a8b
Diyorbek-2022 <111636587+Diyorbek-2022@users.noreply.github.com>
2025-07-31 20:45:07 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot Token
+BOT_TOKEN = env.str("LINK")  # Bot Token
 NGROK_TUNNEL_URL = env.str("NGROK_TUNNEL_URL")  # Bot Token
 
 # uvicorn main:app --host 0.0.0.0 --port 8000 --reload
